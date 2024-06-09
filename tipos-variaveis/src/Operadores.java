dcfc7ccd92605da8374ea817dcdf0fc75ddf8d51
AntonioFeitosaAm <acgfnubank@gmail.com>
2024-06-08 21:57:22 -0400

    Operadores Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,79 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        //classe Operadores.java
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+                
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        // testes práticos de concatenação
+        /* 
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        */
+        
+        //Unários - operadores realizam incremento, decremento, inveter numeros e booleanos
+        /* 
+        //classe Operadores.java
+        int numero = 5;
+                
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);*/
+
+        //Ternario
+
+        /*
+         // classe Operadores.java
+         /* 
+            int a, b;
+
+            a = 5;
+            b = 6;
+
+            //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+            if(a==b)
+            resultado = "verdadeiro";
+            else
+            resultado = "falso";
+            
+
+            //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+            String resultado = (a==b) ? "verdadeiro" : "false";
+
+            System.out.println(valor);
+            */
+    }
+}
