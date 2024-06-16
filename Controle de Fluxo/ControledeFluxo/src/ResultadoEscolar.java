25f217469190a831dce81392d4fab65bb677fb73
AntonioFeitosaAm <acgfnubank@gmail.com>
2024-06-16 17:58:22 -0400

    Exercicios de Condicionais

---
+++ Controle de Fluxo/ControledeFluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+        if(nota >=7){
+            System.out.println("aluno aprovado e a sua nota é: "+ nota);
+        } 
+        else{
+            System.out.println("Aluno Reprovado, com a nota: " + nota);
+        }
+            
+        
+}
+}
